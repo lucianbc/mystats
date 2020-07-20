64e3d27332e26c489c9030e38bcea003bf335f78
lucianbc <lucianboaca97@gmail.com>
2020-07-20 22:12:01 +0300

    Added fitbit client credentials flow autht.

--- src/fitbit/auth.ts
+++ src/fitbit/auth.ts
@@ -1,11 +1,38 @@
 import axios from "axios";
+import qs from "qs";
+import { debug } from "console";
 
-const x = "asd";
+export interface AuthKey {
+  url: string;
+  apiKey: string;
+  apiSecret: string;
+}
 
-export default async () => {
-  const ppl = await axios.get(
-    "http://dummy.restapiexample.com/api/v1/employees"
-  );
-  console.debug(ppl);
-  return ppl;
-};
+export interface AccessToken {
+  access_token: string;
+  expires_in: number;
+  scope: string;
+  token_type: string;
+}
+
+type Token = string;
+
+export function auth({ url, apiKey, apiSecret }: AuthKey) {
+  async function authenticate() {
+    const token = new Buffer(`${apiKey}:${apiSecret}`).toString("base64");
+    debugger;
+    const payload = {
+      grant_type: "client_credentials",
+      expires_in: 3600,
+    };
+    const result = await axios.post(url, qs.stringify(payload), {
+      headers: {
+        Authorization: `Basic ${token}`,
+        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+      },
+    });
+    return result.data as AccessToken;
+  }
+
+  return { authenticate };
+}
