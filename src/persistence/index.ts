425a01b9ba6344f2771b354d049dc1172ed388d6
lucianbc <lucianboaca97@gmail.com>
2020-10-01 21:43:22 +0100

    Implemented firebase persistence tool

--- src/persistence/index.ts
+++ src/persistence/index.ts
@@ -1,3 +1,20 @@
+import "firebase-admin";
+
 export interface Persistor {
   persist: (path: String, data: object) => Promise<void>;
 }
+
+export const createFirebasePersistor = (
+  firestore: FirebaseFirestore.Firestore
+): Persistor => ({
+  persist: async (path, data) => {
+    await firestore.collection(String(path)).add(data);
+  },
+});
+
+export const createLogPersistor = (): Persistor => ({
+  persist: (path, data) => {
+    console.debug(`Write: ${path} -> ${JSON.stringify(data, null, 2)}`);
+    return Promise.resolve();
+  },
+});
