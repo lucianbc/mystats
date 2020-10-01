5ebe362aa4893aa83e0417f467fb17f216fb87af
lucianbc <lucianboaca97@gmail.com>
2020-10-01 23:31:28 +0100

    Added date validation.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,14 +1,19 @@
 import * as functions from "firebase-functions";
+import moment = require("moment");
+import { DATE_FORMAT } from "./fitbit/metrics";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
 export const myStats = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello, logs", { structuredData: true });
   const date = request.query["date"];
-  response.send("Hello from Firebase! Date is " + date);
+  if (!validate(date as string)) {
+    response
+      .status(400)
+      .send(`Date ${date} is not valid. Please use the format ${DATE_FORMAT}`);
+    return;
+  }
+  response.status(400).send("Hello from Firebase! Date is " + date);
 });
+
+const validate = (date: string | undefined) => {
+  return date && moment(date, DATE_FORMAT, true).isValid();
+};
